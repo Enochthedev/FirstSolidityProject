91cdd24ae8cf0e3858d26aaa4b15a3e55605450a
Enochthedev <enochjesse884@gmail.com>
2025-01-14 11:10:09 +0100

    creating a calculator contract

--- contracts/hello.sol
+++ contracts/hello.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract Hello {
+contract Test {
 
-    string public welcome = "Hello world";
+    uint8 MAX_SUPPLY = 10;
+
+    uint256 WAIT_TIME = 1 days;
 }
